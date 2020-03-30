e06c38be4db886987fd883bdea5612745d35c0d6
ArtemPankin <pankin@mpipz.mpg.de>
2020-03-30 21:50:25 +0200

    routine_300320

---
+++ README.rst
@@ -0,0 +1,8 @@
+========================================
+Snakemake pipeline for de novo genome assembly using a combination of long and short read sequencing data (ONT + 10x Illumina + HiFi PacBio Isoseq + Bionano OpticalMap)
+========================================
+
+.. contents:: **Table of Contents**
+
+Snakemake pipeline
+========================================
---
+++ scripts/submit_lsf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+CLUSTER_CMD=("bsub -q {cluster.queue} -n {cluster.nCPUs} -R {cluster.resources} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.name}")
+#JOB_NAME="$1"
+
+#bsub -R "rusage[mem=1000]" \
+#  -M 1000 \
+#  -o logs/cluster_"$JOB_NAME".o \
+#  -e logs/cluster_"$JOB_NAME".e \
+#  -J "$JOB_NAME" \
+  snakemake --cluster-config cluster.yaml \
+    --rerun-incomplete \
+    --latency-wait 2 \
+    --jobs 10 \
+    --cluster "${CLUSTER_CMD}"
+
+exit 0
